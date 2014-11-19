7d6275dbeed72464d15420ce044c691dcd6c899c
madalinaFils <madalina_stan92@yahoo.com>
2014-11-19 11:23:35 +0200

    Create Imagine.java

---
+++ Imagine.java
@@ -0,0 +1,38 @@
+package com.example.listaimagini;
+
+public class Image {
+
+	int id_photo_1;
+	int id_photo_2;
+	int id_photo_3;
+	
+	
+	public Image(int id_photo_1, int id_photo_2, int id_photo_3) {
+		super();
+		this.id_photo_1 = id_photo_1;
+		this.id_photo_2 = id_photo_2;
+		this.id_photo_3 = id_photo_3;
+	}
+	public int getId_photo_1() {
+		return id_photo_1;
+	}
+	public void setId_photo_1(int id_photo_1) {
+		this.id_photo_1 = id_photo_1;
+	}
+	public int getId_photo_2() {
+		return id_photo_2;
+	}
+	public void setId_photo_2(int id_photo_2) {
+		this.id_photo_2 = id_photo_2;
+	}
+	public int getId_photo_3() {
+		return id_photo_3;
+	}
+	public void setId_photo_3(int id_photo_3) {
+		this.id_photo_3 = id_photo_3;
+	}
+	
+	
+	
+	
+}
